ddd80519118bd292560c089509eb02385bd3699a
gdavid04 <gabordavid04@gmail.com>
2019-05-31 20:54:52 +0200

    Cleanup

--- src/main/java/gdavid/davidsmod/block/ModBlocks.java
+++ src/main/java/gdavid/davidsmod/block/ModBlocks.java
@@ -1,7 +1,7 @@
 package gdavid.davidsmod.block;
 
-import gdavid.davidsmod.util.CreateBlockUtil;
-import gdavid.davidsmod.util.RegUtil;
+import static gdavid.davidsmod.util.CreateBlockUtil.*;
+import static gdavid.davidsmod.util.RegUtil.*;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
@@ -15,47 +15,47 @@ import net.minecraft.world.World;
 public class ModBlocks {
 	
 	// no ItemBlock, these need a special item
-	public static final BlockEnderCrop enderCrop = RegUtil.regBlock(false, new BlockEnderCrop());
+	public static final BlockEnderCrop enderCrop = regBlock(false, new BlockEnderCrop());
 	
 	// these have a normal ItemBlock
-	public static final Block stoneTile = RegUtil.regBlock(CreateBlockUtil.create("stone_tile", Material.ROCK, MapColor.STONE, 1.5f, 10.0f));
-	public static final Block flint = RegUtil.regBlock(CreateBlockUtil.adapt("flint_block", new BlockDFalling(Material.ROCK, MapColor.STONE, -8356741).setSoundType(SoundType.GROUND).setHardness(1.0F)));
-	public static final Block smoothOak = RegUtil.regBlock(CreateBlockUtil.create("smooth_oak_wood", Material.WOOD, SoundType.WOOD, 2.0F, 5.0F).setFlammability(20));
-	public static final Block oakPanel = RegUtil.regBlock(CreateBlockUtil.create("oak_panel", Material.WOOD, SoundType.WOOD, 2.0F, 5.0F).setFlammability(20));
-	public static final Block oakPanelStack = RegUtil.regBlock(CreateBlockUtil.create("oak_panel_stack", Material.WOOD, SoundType.WOOD, 2.5F, 8.0F).setFlammability(20));
+	public static final Block stoneTile = regBlock(create("stone_tile", Material.ROCK, MapColor.STONE, 1.5f, 10.0f));
+	public static final Block flint = regBlock(adapt("flint_block", new BlockDFalling(Material.ROCK, MapColor.STONE, -8356741).setSoundType(SoundType.GROUND).setHardness(1.0F)));
+	public static final Block smoothOak = regBlock(create("smooth_oak_wood", Material.WOOD, SoundType.WOOD, 2.0F, 5.0F).setFlammability(20));
+	public static final Block oakPanel = regBlock(create("oak_panel", Material.WOOD, SoundType.WOOD, 2.0F, 5.0F).setFlammability(20));
+	public static final Block oakPanelStack = regBlock(create("oak_panel_stack", Material.WOOD, SoundType.WOOD, 2.5F, 8.0F).setFlammability(20));
 	
-	public static final Block machineBlock = RegUtil.regBlock(CreateBlockUtil.create("machine_block", Material.ROCK, 3.5f));
+	public static final Block machineBlock = regBlock(create("machine_block", Material.ROCK, 3.5f));
 	
-	public static final Block7segDisplay display7seg = RegUtil.regBlock(CreateBlockUtil.adapt("7seg_display", new Block7segDisplay()));
-	public static final BlockBarDisplay displayBar = RegUtil.regBlock(CreateBlockUtil.adapt("bar_display", new BlockBarDisplay()));
-	public static final BlockRedstoneRod redstoneRod = RegUtil.regBlock(CreateBlockUtil.adapt("redstone_rod", new BlockRedstoneRod()));
-	public static final BlockBlockBreaker blockBreaker = RegUtil.regBlock(CreateBlockUtil.adapt("block_breaker", new BlockBlockBreaker(2)));
-	public static final BlockMechanicalFangs mechanicalFangs = RegUtil.regBlock(CreateBlockUtil.adapt("mechanical_fangs", new BlockMechanicalFangs()));
-	public static final BlockTouchSensor touchSensor = RegUtil.regBlock(CreateBlockUtil.adapt("touch_sensor", new BlockTouchSensor()));
-	public static final BlockBlockUpdateRouter blockUpdateRoutingStone = (BlockBlockUpdateRouter) RegUtil.regBlock(CreateBlockUtil.adapt("block_update_routing_stone", new BlockBlockUpdateRouter(Material.ROCK, MapColor.STONE)).setHardness(3.0F));
-	public static final BlockRedstoneCapacitor redstoneCapacitor = RegUtil.regBlock(CreateBlockUtil.adapt("redstone_capacitor", new BlockRedstoneCapacitor()));
-	public static final BlockPressurePlateInStone pressurePlateInStone = RegUtil.regBlock(CreateBlockUtil.adapt("pressure_plate_in_stone", new BlockPressurePlateInStone()));
+	public static final Block7segDisplay display7seg = regBlock(adapt("7seg_display", new Block7segDisplay()));
+	public static final BlockBarDisplay displayBar = regBlock(adapt("bar_display", new BlockBarDisplay()));
+	public static final BlockRedstoneRod redstoneRod = regBlock(adapt("redstone_rod", new BlockRedstoneRod()));
+	public static final BlockBlockBreaker blockBreaker = regBlock(adapt("block_breaker", new BlockBlockBreaker(2)));
+	public static final BlockMechanicalFangs mechanicalFangs = regBlock(adapt("mechanical_fangs", new BlockMechanicalFangs()));
+	public static final BlockTouchSensor touchSensor = regBlock(adapt("touch_sensor", new BlockTouchSensor()));
+	public static final BlockBlockUpdateRouter blockUpdateRoutingStone = (BlockBlockUpdateRouter) regBlock(adapt("block_update_routing_stone", new BlockBlockUpdateRouter(Material.ROCK, MapColor.STONE)).setHardness(3.0F));
+	public static final BlockRedstoneCapacitor redstoneCapacitor = regBlock(adapt("redstone_capacitor", new BlockRedstoneCapacitor()));
+	public static final BlockPressurePlateInStone pressurePlateInStone = regBlock(adapt("pressure_plate_in_stone", new BlockPressurePlateInStone()));
 	
-	public static final BlockBlockDetector waterDetector = (BlockBlockDetector) RegUtil.regBlock(CreateBlockUtil.adapt("water_detector", new BlockBlockDetector(Material.ROCK, MapColor.STONE, (World world, BlockPos pos) -> {
+	public static final BlockBlockDetector waterDetector = (BlockBlockDetector) regBlock(adapt("water_detector", new BlockBlockDetector(Material.ROCK, MapColor.STONE, (World world, BlockPos pos) -> {
 		Block block = world.getBlockState(pos).getBlock();
 		return block == Blocks.WATER || block == Blocks.FLOWING_WATER;
 	})).setHardness(3.5F));
-	public static final BlockBlockDetector lavaDetector = (BlockBlockDetector) RegUtil.regBlock(CreateBlockUtil.adapt("lava_detector", new BlockBlockDetector(Material.ROCK, MapColor.STONE, (World world, BlockPos pos) -> {
+	public static final BlockBlockDetector lavaDetector = (BlockBlockDetector) regBlock(adapt("lava_detector", new BlockBlockDetector(Material.ROCK, MapColor.STONE, (World world, BlockPos pos) -> {
 		Block block = world.getBlockState(pos).getBlock();
 		return block == Blocks.LAVA || block == Blocks.FLOWING_LAVA;
 	})).setHardness(3.5F));
-	public static final BlockBlockDetector blockDetector = (BlockBlockDetector) RegUtil.regBlock(CreateBlockUtil.adapt("block_detector", new BlockBlockDetector(Material.ROCK, MapColor.STONE, (World world, BlockPos pos) -> {
+	public static final BlockBlockDetector blockDetector = (BlockBlockDetector) regBlock(adapt("block_detector", new BlockBlockDetector(Material.ROCK, MapColor.STONE, (World world, BlockPos pos) -> {
 		IBlockState state = world.getBlockState(pos);
 		return !state.getBlock().isAir(state, world, pos);
 	})).setHardness(3.5F));
 	
-	public static final BlockDimensionGate endGate = (BlockDimensionGate) RegUtil.regBlock(CreateBlockUtil.adapt("end_gate", new BlockDimensionGate(1, Material.ROCK, MapColor.BLACK).setHardness(5.0F)));
+	public static final BlockDimensionGate endGate = (BlockDimensionGate) regBlock(adapt("end_gate", new BlockDimensionGate(1, Material.ROCK, MapColor.BLACK).setHardness(5.0F)));
 	
-	public static final BlockChargedIron chargedIron = RegUtil.regBlock(CreateBlockUtil.adapt("charged_iron_block", new BlockChargedIron()));
-	public static final BlockChargedRock chargedRock = RegUtil.regBlock(CreateBlockUtil.adapt("charged_rock", new BlockChargedRock()));
+	public static final BlockChargedIron chargedIron = regBlock(adapt("charged_iron_block", new BlockChargedIron()));
+	public static final BlockChargedRock chargedRock = regBlock(adapt("charged_rock", new BlockChargedRock()));
 	
-	public static final BlockDOre fossils = (BlockDOre) RegUtil.regBlock(BlockDOre.create("fossils", Items.BONE, 1, 4, 0, 3, 1).setHardness(3.0F));
-	public static final BlockDOre quartzOre = (BlockDOre) RegUtil.regBlock(BlockDOre.create("quartz_ore", Items.QUARTZ, 2, 3, 0, 3, 2).setHardness(4.5F));
+	public static final BlockDOre fossils = (BlockDOre) regBlock(BlockDOre.create("fossils", Items.BONE, 1, 4, 0, 3, 1).setHardness(3.0F));
+	public static final BlockDOre quartzOre = (BlockDOre) regBlock(BlockDOre.create("quartz_ore", Items.QUARTZ, 2, 3, 0, 3, 2).setHardness(4.5F));
 	
 	public static void reg() {
 		// used to explicitly initialize static fields, thus registering the blocks
--- src/main/java/gdavid/davidsmod/item/ModItems.java
+++ src/main/java/gdavid/davidsmod/item/ModItems.java
@@ -1,28 +1,28 @@
 package gdavid.davidsmod.item;
 
 import gdavid.davidsmod.api.DavidsModCrafting;
-import gdavid.davidsmod.util.CreateItemUtil;
-import gdavid.davidsmod.util.RegUtil;
+import static gdavid.davidsmod.util.CreateItemUtil.*;
+import static gdavid.davidsmod.util.RegUtil.*;
 
 import net.minecraft.item.Item;
 
 public class ModItems {
 	
-	public static final ItemRedstoneMeter redstoneMeter = RegUtil.regItem(CreateItemUtil.adapt("redstone_meter", new ItemRedstoneMeter()));
-	public static final ItemRedstoneMeterWithDisplay redstoneMeterWithDisplay = RegUtil.regItem(CreateItemUtil.adapt("redstone_meter_with_display", new ItemRedstoneMeterWithDisplay()));
-	public static final Item moduleBase = RegUtil.regItem(CreateItemUtil.create("module_base", 16));
-	public static final Item moduleDisplayBasic = RegUtil.regItem(CreateItemUtil.create("basic_display_module", 16));
-	public static final Item moduleDisplay = RegUtil.regItem(CreateItemUtil.create("display_module", 16));
-	public static final Item moduleInput = RegUtil.regItem(CreateItemUtil.create("input_module", 16));
-	public static final Item moduleInput2 = RegUtil.regItem(CreateItemUtil.create("tier2_input_module", 16));
-	public static final ItemBotProgrammer botProgrammer = RegUtil.regItem(CreateItemUtil.adapt("bot_programmer", new ItemBotProgrammer()));
-	public static final ItemEnderSeed enderSeed = RegUtil.regItem(CreateItemUtil.adapt("ender_seed", new ItemEnderSeed()));
-	public static final ItemInfinityPickaxe infinityPickaxe = RegUtil.regItem(CreateItemUtil.adapt("infinity_pickaxe", new ItemInfinityPickaxe()));
-	public static final Item bedrockDust = RegUtil.regItem(CreateItemUtil.create("bedrock_dust"));
-	public static final ItemBlockTransformer mossBall = RegUtil.regItem(CreateItemUtil.adapt("moss_ball", new ItemBlockTransformer(DavidsModCrafting.mossBall)));
-	public static final Item voidStar = RegUtil.regItem(CreateItemUtil.create("void_star"));
-	public static final Item sand = RegUtil.regItem(CreateItemUtil.create("sand"));
-	public static final Item bloodBottle = RegUtil.regItem(CreateItemUtil.create("bottle_of_blood", 1));
+	public static final ItemRedstoneMeter redstoneMeter = regItem(adapt("redstone_meter", new ItemRedstoneMeter()));
+	public static final ItemRedstoneMeterWithDisplay redstoneMeterWithDisplay = regItem(adapt("redstone_meter_with_display", new ItemRedstoneMeterWithDisplay()));
+	public static final Item moduleBase = regItem(create("module_base", 16));
+	public static final Item moduleDisplayBasic = regItem(create("basic_display_module", 16));
+	public static final Item moduleDisplay = regItem(create("display_module", 16));
+	public static final Item moduleInput = regItem(create("input_module", 16));
+	public static final Item moduleInput2 = regItem(create("tier2_input_module", 16));
+	public static final ItemBotProgrammer botProgrammer = regItem(adapt("bot_programmer", new ItemBotProgrammer()));
+	public static final ItemEnderSeed enderSeed = regItem(adapt("ender_seed", new ItemEnderSeed()));
+	public static final ItemInfinityPickaxe infinityPickaxe = regItem(adapt("infinity_pickaxe", new ItemInfinityPickaxe()));
+	public static final Item bedrockDust = regItem(create("bedrock_dust"));
+	public static final ItemBlockTransformer mossBall = regItem(adapt("moss_ball", new ItemBlockTransformer(DavidsModCrafting.mossBall)));
+	public static final Item voidStar = regItem(create("void_star"));
+	public static final Item sand = regItem(create("sand"));
+	public static final Item bloodBottle = regItem(create("bottle_of_blood", 1));
 	
 	public static void reg() {
 		// used to explicitly initialize static fields, thus registering the items
