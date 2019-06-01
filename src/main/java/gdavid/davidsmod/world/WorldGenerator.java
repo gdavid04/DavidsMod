52167d75851312d2da8228b53a6224a7c50b2128
gdavid04 <gabordavid04@gmail.com>
2019-06-01 11:26:44 +0200

    Added lava generation on bottom of the world

--- src/main/java/gdavid/davidsmod/world/WorldGenerator.java
+++ src/main/java/gdavid/davidsmod/world/WorldGenerator.java
@@ -1,9 +1,11 @@
 package gdavid.davidsmod.world;
 
 import java.util.Random;
+import java.util.function.BiPredicate;
 
 import gdavid.davidsmod.block.ModBlocks;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
@@ -16,9 +18,11 @@ public class WorldGenerator implements IWorldGenerator {
 	
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+		int x = chunkX << 4, z = chunkZ << 4;
 		if (world.provider.getDimensionType() == DimensionType.OVERWORLD) {
-			generateOre(ModBlocks.fossils.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 40, 128, 1 + random.nextInt(10), 4);
-			generateOre(ModBlocks.quartzOre.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 0, 82, 2 + random.nextInt(4), 20);
+			generateOre(ModBlocks.fossils.getDefaultState(), world, random, x, z, 40, 128, 1 + random.nextInt(10), 4);
+			generateOre(ModBlocks.quartzOre.getDefaultState(), world, random, x, z, 0, 82, 2 + random.nextInt(4), 20);
+			floodLayer(Blocks.LAVA.getDefaultState(), world, random, x + 8, z + 8, 1, 5, 0.6f, (w, p) -> w.getBlockState(p).getBlock() != Blocks.BEDROCK);
 		}
 	}
 	
@@ -28,6 +32,19 @@ public class WorldGenerator implements IWorldGenerator {
 			BlockPos pos = new BlockPos(x + random.nextInt(16), minY + random.nextInt(deltaY), z + random.nextInt(16));
 			WorldGenMinable generator = new WorldGenMinable(ore, size);
 			generator.generate(world, random, pos);
+		}
+	}
+	
+	private void floodLayer(IBlockState fill, World world, Random random, int x, int z, int minY, int maxY, float chance, BiPredicate<World, BlockPos> replace) {
+		for (int y = minY; y <= maxY; y++) {
+			for (int cx = x; cx < x + 16; cx++) {
+				for (int cz = z; cz < z + 16; cz++) {
+					BlockPos pos = new BlockPos(cx,  y, cz);
+					if (random.nextFloat() <= chance && replace.test(world, pos)) {
+						world.setBlockState(pos, fill, 2);
+					}
+				}
+			}
 		}
 	}
 	
